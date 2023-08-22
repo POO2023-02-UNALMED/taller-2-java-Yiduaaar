1f685e1c8e06777588d7642c43e4a26e10b7dd6c
Yiduaaar <124937930+Yiduaaar@users.noreply.github.com>
2023-08-22 03:23:11 -0500

    Create Motor.java

---
+++ src/main/java/Motor.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Motor {
+    int numeroCilindros;
+    String tipo;
+    int registro;
+    public void cambiarRegistro(int registro){
+        this.registro = registro;
+
+
+    }
+    public void asignarTipo(String tipo){
+        if(tipo.equals("gasolina") || tipo.equals("electrico")){
+            this.tipo = tipo;
+        }
+    }
+}
